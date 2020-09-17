382926c65299e1f41046ebaad08428d181f7de3c
Felipe André <fass@icomp.ufam.edu.br>
2020-09-17 13:51:49 -0400

    DocumentAdapter updated and moved to ui.DocumentChangeListener

---
+++ Phills Libs/src/com/phill/libs/DocumentChangeListener.java
@@ -0,0 +1,28 @@
+package com.phill.libs;
+
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+/** This interface is only intended to use with {@link DocumentListener} lambdas, as
+ *  this one does not allow the programmer to do it.
+ *  NOTE: when using {@link DocumentListener}, the methods 'insertUpdate' and 'removeUpdate'
+ *  are redirected to the 'changedUpdate' method.
+ * @author Felipe André - felipeandresouza@hotmail.com
+ * @version 1.5, 17/SEP/2020 */
+@FunctionalInterface
+public interface DocumentChangeListener extends DocumentListener {
+
+	@Override
+	public default void insertUpdate(DocumentEvent event) {
+		changedUpdate(event);
+	}
+	
+	@Override
+	public default void removeUpdate(DocumentEvent event) {
+		changedUpdate(event);
+	}
+	
+	@Override
+	public void changedUpdate(DocumentEvent event);
+
+}
