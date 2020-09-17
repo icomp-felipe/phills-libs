339654363a3093d01de723d0fcc17cf4a913cfb1
Felipe André <fass@icomp.ufam.edu.br>
2020-09-17 03:15:43 -0400

    TableMouseListener updated and renamed to JTableMouseListener

---
+++ Phills Libs/src/com/phill/libs/table/JTableMouseListener.java
@@ -0,0 +1,33 @@
+package com.phill.libs.table;
+
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JTable;
+
+/** This class is useful when you need to open a popup menu
+ *  using the mouse right click in a specific {@link JTable} row.
+ *  @author Felipe André - felipeandresouza@hotmail.com
+ *  @version 1.5, 17/SEP/2020 */
+public class JTableMouseListener extends MouseAdapter {
+	
+	private final JTable table;
+	
+	public JTableMouseListener(JTable table) {
+		this.table = table;
+	}
+	
+	/** Selects the nearest {@link JTable} row pointed by the last mouse click event.
+	 *  If the algorithm can't resolve the nearest row, it simply ignores the mouse event. */
+    @Override
+    public void mouseReleased(MouseEvent event) {
+    	
+        Point point = event.getPoint();
+        int currentRow = table.rowAtPoint(point);
+        
+        if (currentRow != -1)
+        	table.setRowSelectionInterval(currentRow, currentRow);
+        
+    }
+    
+}
