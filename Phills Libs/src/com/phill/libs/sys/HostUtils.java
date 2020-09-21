e01ec3621bdadbd1c90cb4ea5869fe534e7f3bb3
Felipe André <fass@icomp.ufam.edu.br>
2020-09-21 01:49:54 -0400

    Added getScreenWidth method to HostUtils

--- Phills Libs/src/com/phill/libs/sys/HostUtils.java
+++ Phills Libs/src/com/phill/libs/sys/HostUtils.java
@@ -1,13 +1,20 @@
 package com.phill.libs.sys;
 
 import java.awt.Desktop;
+import java.awt.Toolkit;
 import java.net.URL;
 
 /** Contains methods to deal with host OS based features.
  *  @author Felipe André - felipeandresouza@hotmail.com
- *  @version 1.5, 17/SEP/2020 */
+ *  @version 2.0, 20/SEP/2020 */
 public class HostUtils {
 
+	/** Recovers the system current screen resolution width.
+	 *  @return System screen width in pixels. */
+	public static int getScreenWidth() {
+	    return Toolkit.getDefaultToolkit().getScreenSize().width;
+	}
+	
 	/** Opens a URL using host's default web browser.
 	 *  @param url - URL */
 	public static void openURL(final String url) {
