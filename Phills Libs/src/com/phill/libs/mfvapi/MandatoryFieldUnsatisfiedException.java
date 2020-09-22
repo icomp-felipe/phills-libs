05d67dc70c7de88f89cb94acbd47b7fd0cf456aa
Felipe André <fass@icomp.ufam.edu.br>
2020-09-22 02:24:50 -0400

    MVF API exception reviewed and commented

---
+++ Phills Libs/src/com/phill/libs/mfvapi/MandatoryFieldUnsatisfiedException.java
@@ -0,0 +1,15 @@
+package com.phill.libs.mfvapi;
+
+/** MFV API exception available to be thrown.
+ *  @author Felipe André - felipeandresouza@hotmail.com
+ *  @version 1.5, 22/SEP/2020 */
+public class MandatoryFieldUnsatisfiedException extends Exception {
+
+	private static final long serialVersionUID = -6798511668473411372L;
+
+	/** Constructor simply redirecting the given <code>message</code> to {@link Exception} superclass. */
+	public MandatoryFieldUnsatisfiedException(final String message) {
+		super(message);
+	}
+
+}
